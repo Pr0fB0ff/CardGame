cd513aa85fc5f4b9a17f0d9293cd83333cef7933
Pr0fB0ff <msereboff@gmail.com>
2017-11-05 13:03:23 -0800

    Initial Commit

---
+++ CardGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CardGame
+//
+//  Created by Marcel on 11/5/17.
+//  Copyright © 2017 Marcel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
